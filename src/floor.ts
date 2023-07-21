27dcf4f8d947d14eb9848057399b88507b1887ad
Viktor Mörsell <viktor@upper.st>
2023-07-21 22:49:54 +0200

    Add floor interior

--- src/floor.ts
+++ src/floor.ts
@@ -2,11 +2,24 @@
 
 import * as Phaser from 'phaser';
 
-import { notBuiltTextureName, floorTextureName } from './game';
+import { notBuiltTextureName, floorTextureName, InteriorTexture } from './game';
 
-export class Floor extends Phaser.GameObjects.Image {
-    constructor(scene: Phaser.Scene) {
-        super(scene, 0, 0, notBuiltTextureName);
+export class Floor extends Phaser.GameObjects.Container {
+    private floor: Phaser.GameObjects.Image;
+    private interiorTexture: InteriorTexture;
+
+    constructor(scene: Phaser.Scene, interiorTexture: InteriorTexture) {
+        const floor = new Phaser.GameObjects.Image(
+            scene,
+            0,
+            0,
+            notBuiltTextureName
+        );
+        super(scene, 0, 0, [floor]);
+
+        this.floor = floor;
+        this.interiorTexture = interiorTexture;
+        this.setSize(floor.width, floor.height);
 
         this.setInteractive();
         this.on('pointerdown', () => {
@@ -15,6 +28,18 @@ export class Floor extends Phaser.GameObjects.Image {
     }
 
     build() {
-        this.setTexture(floorTextureName);
+        this.floor.setTexture(floorTextureName);
+
+        const interior = new Phaser.GameObjects.Image(
+            this.scene,
+            // The transform point of a Container is 0x0 and cannot
+            // be changed. Children needs to be positioned positively
+            // or negatively around it.
+            -this.width / 2 + 93,
+            -this.height / 2 + 32,
+            this.interiorTexture
+        );
+        interior.setOrigin(0, 0);
+        this.add(interior);
     }
 }
--- src/game.ts
+++ src/game.ts
@@ -4,6 +4,11 @@ import * as Phaser from 'phaser';
 
 import { Floor } from './floor';
 
+export enum InteriorTexture {
+  Slurry = 'slurry',
+  Coating = 'coating',
+}
+
 export const notBuiltTextureName = 'not_built';
 export const floorTextureName = 'floor';
 
@@ -17,6 +22,8 @@ export default class MainScene extends Phaser.Scene {
     preload() {
         this.load.image(notBuiltTextureName, 'assets/not_built.png');
         this.load.image(floorTextureName, 'assets/floor.png');
+        this.load.image(InteriorTexture.Slurry, 'assets/slurry.png');
+        this.load.image(InteriorTexture.Coating, 'assets/coating.png');
     }
 
     create() {
@@ -25,17 +32,15 @@ export default class MainScene extends Phaser.Scene {
         this.cameras.main.setZoom(1);
 
         this.floors = [
-            new Floor(this),
-            new Floor(this),
-            new Floor(this),
-            new Floor(this),
-            new Floor(this),
+            new Floor(this, InteriorTexture.Slurry),
+            new Floor(this, InteriorTexture.Coating),
         ];
 
         this.floors.forEach((floor, i) => {
-            floor.setOrigin(0.5, 1);
             floor.setX(this.game.scale.width / 2);
-            floor.setY(this.game.scale.height - floor.height * i);
+            floor.setY(
+                this.game.scale.height - floor.height / 2 - floor.height * i
+            );
             console.log(floor.height);
             this.add.existing(floor);
         });
